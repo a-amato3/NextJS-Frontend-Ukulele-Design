f80b65c73edc289f7b8bb596fcba7dad10769e29
a-amato3 <aaronamato@hotmail.com>
2022-06-13 16:07:15 +0100

    add build folder

--- public/Uke_engrave/android/app/src/main/kotlin/com/example/uke_engraver/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.uke_engraver
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- public/Uke_engrave/lib/core/services/service_functions/provider_functions.dart
+++
@@ -1,9 +0,0 @@
-import 'package:provider/provider.dart';
-import 'package:uke_engraver/locator.dart';
-import 'package:uke_engraver/core/services/providers/new_provider.dart';
-
-class ProviderFunctions {
-  final _provider = locator<UkeProvider>();
-
-
-}
