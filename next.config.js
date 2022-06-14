29780e563323dc0ae3a7715d5455bbd5d94a471e
a-amato3 <aaronamato@hotmail.com>
2022-06-14 12:46:55 +0100

    service worker working

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  rewrites: async () => {
+    return [
+      {
+        source: "/builder",
+        destination: "/build/web/index.html",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
